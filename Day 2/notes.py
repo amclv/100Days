59fc47565da4174143bb6018a706a77e14614816
Aaron Cleveland <toscleveland@gmail.com>
2021-02-09 03:06:21 -0600

    Day 2 | Completed | Tip Calculator

---
+++ Day 2/notes.py
@@ -0,0 +1,90 @@
+# Data Types
+
+# Strings
+
+# Get the first letter of the string
+# by subscripting the position in brackets
+# print("Hello"[0])
+
+# Integers
+# print(123456789)
+
+# num = 123_456_789
+# print(num)
+
+# Float
+
+# 3.14159
+
+# Boolean
+
+# True
+# False
+
+# Printing type will give you the type of the variable
+# print(type(num_char))
+
+# Converting number into a string
+# num_char = len(input("What is your name?\n"))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters")
+
+# a = str(456)
+# a = float(123)
+# print(type(a))
+
+# first_digit = int(input("first number: "))
+# second_digit = int(input("second number: "))
+
+# sum_digits = first_digit + second_digit
+# print(sum_digits)
+
+# Math in Python
+# Pemdas (Paranthesis, Exponents, Multiply, Division, Add, Subtract)
+# 3 + 5 # Addition
+# 7 - 3 # Subtract
+# 3 * 2 # Multiply
+# 6 / 3 # Division
+# 2 ** 2 # Exponents
+
+# print("BMI CALCULATION! PLEASE ENTER THE ITEMS BELOW!\n")
+
+# height = input("Enter your height in m: ")
+# weight = input("Enter your weight in kg: ")
+
+# bmi = int(weight) / float(height) ** 2
+# bmi_integer = int(bmi)
+# print(bmi_integer)
+
+# Rounding
+# print(round(8 / 3))
+
+# Adding a number after the comma specifies the place for rounding.
+# print(round(2.555555555, 2))
+
+# result = 4 / 2
+# result /= 2
+# print(result)
+
+# User scores a point
+# score = 0
+# score = score + 1
+# Or
+# score += 1
+# print(score)
+
+
+# f String
+# score = 0
+# height = 1.8
+# isWinning = True
+# print(f"Your score is {score}, your height is {height} and you are winning is {isWinning}")
+
+# age = input("What is your current age?\n")
+
+# time_left = 90 - int(age)
+# months_left = time_left * 12
+# weeks_left = time_left * 52
+# days_left = time_left * 365
+
+# print(f"You have {days_left} days, {weeks_left} weeks, {months_left} months left.")
