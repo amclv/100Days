eb42e00c979330df738083069a1cfe8f3650457a
Aaron Cleveland <toscleveland@gmail.com>
2021-02-12 23:41:59 -0600

    finished notes for day 5

---
+++ Day 5/notes.py
@@ -0,0 +1,50 @@
+# Loops
+# ---------------------->
+
+# For Loop
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+
+# student_heights = input("Input a list of student heights: ").split()
+# for n in range(0, len(student_heights)):
+#     student_heights[n] = int(student_heights[n])
+#     print(student_heights)
+
+# count = 0
+# for height in student_heights:
+#     count += height
+#     print(count)
+
+# student_scores = [100, 105, 110, 120]
+# high_score = 0
+# for score in student_scores:
+#     if score > high_score:
+#         high_score = score
+#         print(f"Highest score: {high_score}")
+
+
+# print("Welcome to PyPassword!\n")
+# char_length = input("How man letters would you like in your password?\n")
+
+# total = 0
+# for number in range(1, 101):
+#     total += number
+#     print(total)
+
+# total = 0
+# for number in range(1, 101):
+#     if number % 2 == 0:
+#         total += number
+#         print(total)
+
+# total = 0
+# for number in range(1, 101):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("FizzBuzz")
+#     elif number % 3 == 0:
+#         print("Fizz")
+#     elif number % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(number)
