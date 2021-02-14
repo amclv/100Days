4bb76fa80bf1d6b5f268f8b03ad66bdfc803451c
Aaron Cleveland <toscleveland@gmail.com>
2021-02-14 15:00:31 -0600

    Day 6 | Completed | whileloops

---
+++ Day 6/notes.py
@@ -0,0 +1,24 @@
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+
+# Functions
+# Define the function you want
+# def my_function():
+#     print("hello")
+#     print("bye")
+# Call the function   
+# my_function()
+
+number_of_hurdles = 6
+count = 0
+# while number_of_hurdles > 0:
+#     count += 1
+#     # number_of_hurdles -= 1
+#     print(count)
+    
+at_goal = False
+while not at_goal:
+    count += 1
+    # number_of_hurdles -= 1
+    print(count)
